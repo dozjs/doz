3e08dcc22ece8a526f1f9347fcd1c1fc3f155bf9
fabioricali <ricalifabio@gmail.com>
2018-05-17 18:55:50 +0200

    v0.0.25

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 0.0.24  
+// [DOZ]  Build version: 0.0.25  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
