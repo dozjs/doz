4143402c57566b557948f9f4e9c2b15075156080
fabioricali <ricalifabio@gmail.com>
2018-07-04 13:18:03 +0200

    v1.3.2

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 1.3.1  
+// [DOZ]  Build version: 1.3.2  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -1745,7 +1745,7 @@ module.exports.collection = __webpack_require__(1);
 module.exports.update = __webpack_require__(11).updateElement;
 module.exports.transform = __webpack_require__(8).transform;
 module.exports.html = __webpack_require__(4);
-module.exports.version = '1.3.1';
+module.exports.version = '1.3.2';
 
 /***/ }),
 /* 14 */
