e34a273253f5d068cfe86ebd00423aa483843a34
fabioricali <ricalifabio@gmail.com>
2018-08-02 13:24:15 +0200

    fix scoped style issue with hmr

--- src/component/style.js
+++ src/component/style.js
@@ -1,32 +1,19 @@
-const toInlineStyle = require('../utils/to-inline-style');
+const composeStyle = require('../utils/compose-style');
 
 function scoped(instance) {
     if (typeof instance.style !== 'object') return;
 
-    function composeStyle(style) {
-        let out = '';
-        Object.keys(style).forEach(key => {
-            if (/^@media/.test(key)) {
-                out += `${key} {${composeStyle(style[key])}}`
-            } else {
-                let properties = toInlineStyle(style[key], false);
-                key = `${tag} ${key.replace(/(,)/g, `$1${tag} `)}`;
-                out += `${key}{${properties}} `;
-            }
-        });
-        return out;
-    }
-
-    const tag = instance.tag;
     const styleId = `${instance.tag}--style`;
+    const styleExists = document.querySelector(`#${styleId}`);
 
-    if (document.querySelector(`#${styleId}`)) return;
-
-    const styleEl = document.createElement('style');
-    styleEl.id = styleId;
-    styleEl.innerHTML = composeStyle(instance.style);
-
-    document.head.appendChild(styleEl);
+    if (styleExists) {
+        styleExists.innerHTML = composeStyle(instance.style, instance.tag);
+    } else {
+        const styleEl = document.createElement('style');
+        styleEl.id = styleId;
+        styleEl.innerHTML = composeStyle(instance.style, instance.tag);
+        document.head.appendChild(styleEl);
+    }
 }
 
 module.exports = {
