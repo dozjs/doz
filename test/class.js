f7bb34a7342d1e56617ed88d809d75ac8872fe40
fabioricali <ricalifabio@gmail.com>
2018-08-22 16:47:45 +0200

    experiment

---
+++ test/class.js
@@ -0,0 +1,54 @@
+class Component {
+
+    constructor() {
+
+    }
+
+    static define() {
+
+    }
+
+}
+
+class Doz {
+    constructor(cfg, Cmp) {
+        this.cmp = new Cmp();
+        console.log(this.cmp.template());
+    }
+}
+
+function h(str) {
+
+}
+
+class MyComponent extends Component{
+
+    constructor() {
+        super();
+        this.props = {
+            name: 'Mondo'
+        }
+    }
+
+    template() {
+        return h`
+            <div>
+                ciao ${this.props.name}
+            </div>
+        `
+    }
+
+    onMount() {
+
+    }
+}
+
+Component.define('my-component', MyComponent);
+
+new Doz({
+    root: '#app'
+}, class extends Component {
+    template() {
+        return `<my-component></my-component>`
+    }
+});
