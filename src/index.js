9b653dded7158858f79c0ff9f4ff58a2ab3f1414
Fabio Ricali <ricalifabio@gmail.com>
2018-09-08 10:24:40 +0200

    improve properties

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
 const Doz = require('./Doz');
-const _component = require('./component');
+const collection = require('./collection');
+const component = require('./component');
+const Component = require('./component/Component').Component;
+const h = require('./vdom/h');
+const mixin = require('./component/global-mixin');
 
 Object.defineProperties(Doz, {
-   collection: {
-       value: require('./collection'),
-       enumerable: true
-   },
-   Component: {
-       value: require('./component/Component').Component,
-       enumerable: true
-   },
-   component: {
-       value: _component,
-       enumerable: true
-   },
-   define: {
-       value: _component,
-       enumerable: true
-   },
-   h: {
-       value: require('./vdom/h'),
-       enumerable: true
-   },
-   mixin: {
-       value: require('./component/global-mixin'),
-       enumerable: true
-   },
-   version: {
-       value: '[AIV]{version}[/AIV]',
-       enumerable: true
-   }
+    collection: {
+        value: collection,
+        enumerable: true
+    },
+    Component: {
+        value: Component,
+        enumerable: true
+    },
+    component: {
+        value: component,
+        enumerable: true
+    },
+    define: {
+        value: component,
+        enumerable: true
+    },
+    h: {
+        value: h,
+        enumerable: true
+    },
+    mixin: {
+        value: mixin,
+        enumerable: true
+    },
+    version: {
+        value: '[AIV]{version}[/AIV]',
+        enumerable: true
+    }
 });
 
 module.exports = Doz;
