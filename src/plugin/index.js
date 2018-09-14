08d675117f996cb0b2ead004d1fec909be1e0a30
fabioricali <ricalifabio@gmail.com>
2018-09-14 16:25:15 +0200

    add options to plugins

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,15 +1,18 @@
 const {registerPlugin, data} = require('../collection');
 
-function use(plugin) {
+function use(plugin, options = {}) {
     if (typeof plugin !== 'function') {
         throw new TypeError('Plugin must be a function');
     }
+
+    plugin['options'] = options;
+
     registerPlugin(plugin);
 }
 
 function load(app) {
     data.plugins.forEach(func => {
-        func(app.constructor, app);
+        func(app.constructor, app, func.options);
     })
 }
 
--- test/doz.use.js
+++ test/doz.use.js
@@ -54,5 +54,47 @@ describe('Doz.use', function () {
             });
 
         });
+
+        it('should be ok, with options', function (done) {
+
+            document.body.innerHTML = `<div id="app"></div>`;
+
+            const myPluginAddCiao = function(Doz, app, opt) {
+                Doz.mixin({
+                    myCiao3() {
+                        return `Ciao ${opt.name}`
+                    }
+                });
+            };
+
+            Doz.use(myPluginAddCiao, {
+                name: 'Blat &'
+            });
+
+            Doz.component('salutation-card', {
+                template() {
+                    return `
+                        <div>${this.myCiao3()} ${this.props.title} ${this.props.name}</div>
+                    `
+                }
+            });
+
+            new Doz({
+                root: '#app',
+                template: `
+                    <salutation-card
+                        title="MR."
+                        name="Doz">
+                    </salutation-card>
+                `
+            });
+
+            setTimeout(()=>{
+                const result = document.body.innerHTML;
+                console.log(result);
+                be.err(done).true(/Ciao Blat/.test(result));
+            }, 100);
+
+        });
     });
 });
