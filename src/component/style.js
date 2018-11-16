e9b2784e1104e7c63e41f2c525df030989057e06
fabioricali <ricalifabio@gmail.com>
2018-11-16 11:25:15 +0100

    add deprecation

--- src/component/style.js
+++ src/component/style.js
@@ -4,6 +4,7 @@ const createStyle = require('../utils/create-style');
 
 function scoped(instance) {
     if (typeof instance.style !== 'object') return;
+    console.warn('Style object is deprecated since 1.8.0, use style tag inside template instead.');
     const cssContent = composeStyle(instance.style, instance.tag);
     return createStyle(cssContent, instance.tag);
 }
