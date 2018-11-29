a167fc61ba97ba4ef3fd5cac6c1338bdd0f5e9e8
fabioricali <ricalifabio@gmail.com>
2018-11-29 19:05:36 +0100

    v1.9.0

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 1.8.4  
+// [DOZ]  Build version: 1.9.0  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -2358,7 +2358,7 @@ Object.defineProperties(Doz, {
         enumerable: true
     },
     version: {
-        value: '1.8.4',
+        value: '1.9.0',
         enumerable: true
     }
 });
