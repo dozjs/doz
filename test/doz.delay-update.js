03309fa1e02a042a645646ae42a460a09919c063
Fabio Ricali <ricalifabio@gmail.com>
2019-02-10 12:20:11 +0100

    add test for delay

---
+++ test/doz.delay-update.js
@@ -0,0 +1,113 @@
+const Doz = require('../index');
+const be = require('bejs');
+
+describe('Doz.delay-update', function () {
+
+    beforeEach(function () {
+        document.body.innerHTML = '';
+        Doz.collection.removeAll();
+    });
+
+    describe('create app', function () {
+
+        it('should be delay of 1000ms with object definition', function (done) {
+
+            document.body.innerHTML = `<div id="app"></div>`;
+
+            Doz.component('my-delay', {
+                delayUpdate: 1000,
+
+                props: {
+                    aNumber: 0
+                },
+
+                template() {
+                    return `
+                        <div>Number: ${this.props.aNumber}</div>
+                    `
+                },
+
+                onMount() {
+                    this.props.aNumber = 5;
+                },
+
+                onBeforeUpdate() {
+                    console.log('onBeforeUpdate');
+                    this.startUpdate = Date.now();
+                },
+
+                onUpdate() {
+                    const totalTime = Date.now() - this.startUpdate;
+                    console.log('onUpdate', totalTime);
+                    if (totalTime >= this.delayUpdate)
+                    done();
+                }
+            });
+
+            new Doz({
+                root: '#app',
+
+                template(h) {
+                    return h`
+                        <my-delay/>
+                    `
+                }
+            });
+
+        });
+
+        it('should be delay of 1000ms with class definition', function (done) {
+
+            document.body.innerHTML = `<div id="app"></div>`;
+
+            const result = [];
+
+            Doz.define('my-delay', class extends Doz.Component {
+
+                constructor(o) {
+                    super(o);
+
+                    this.props = {
+                        aNumber: 0
+                    };
+
+                    this.delayUpdate = 1000;
+
+                }
+
+                template() {
+                    return `
+                        <div>Number: ${this.props.aNumber}</div>
+                    `
+                }
+
+                onMount() {
+                    this.props.aNumber = 5;
+                }
+
+                onBeforeUpdate() {
+                    console.log('onBeforeUpdate');
+                    this.startUpdate = Date.now();
+                }
+
+                onUpdate() {
+                    const totalTime = Date.now() - this.startUpdate;
+                    console.log('onUpdate', totalTime);
+                    if (totalTime >= this.delayUpdate)
+                        done();
+                }
+            });
+
+            new Doz({
+                root: '#app',
+
+                template(h) {
+                    return h`
+                        <my-delay/>
+                    `
+                }
+            });
+
+        });
+    });
+});
