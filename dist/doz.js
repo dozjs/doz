a858b6e601b81faaf66cfeeda91f35152da166d9
Fabio Ricali <f.ricali@xdevel.com>
2019-07-08 19:23:10 +0200

    build

--- dist/doz.js
+++ dist/doz.js
@@ -110,7 +110,7 @@ module.exports = {
         IS_PARENT_METHOD: /^parent.(.*)/,
         IS_STRING_QUOTED: /^"\w+"/,
         IS_SVG: /^svg$/,
-        IS_CLASS: /^(class\s|function\s+_class)|(throw new TypeError\("Cannot call a class)/i,
+        IS_CLASS: /^(class\s|function\s+_class)|(throw new TypeError\("Cannot call a class)|(function.*\.__proto__\|\|Object\.getPrototypeOf\(.*?\))/i,
         GET_LISTENER: /^this.(.*)\((.*)\)/,
         TRIM_QUOTES: /^["'](.*)["']$/,
         THIS_TARGET: /\B\$this(?!\w)/g,
