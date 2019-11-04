304fcd94564c41d65a7359cff2513b126ea467fd
Fabio Ricali <ricalifabio@gmail.com>
2019-11-04 08:49:39 +0100

    build

--- dist/doz.js
+++ dist/doz.js
@@ -2756,6 +2756,15 @@ module.exports = function (strings) {
                         cfg: cmp
                     };
                 }
+
+                // add to local app components
+                if (this.app._components[tagCmp] === undefined) {
+                    this.app._components[tagCmp] = {
+                        tag: tagCmp,
+                        cfg: cmp
+                    };
+                }
+
                 value[i] = tagCmp;
             }
         }
