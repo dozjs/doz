12a4aadcc17af551e48fb202706b6e42e214da68
Fabio Ricali <ricalifabio@gmail.com>
2019-12-14 11:29:50 +0100

    v2.3.0

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 2.2.2  
+// [DOZ]  Build version: 2.3.0  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -3200,7 +3200,7 @@ Object.defineProperties(Doz, {
         value: mapper
     },
     version: {
-        value: '2.2.2',
+        value: '2.3.0',
         enumerable: true
     },
     tag: {
