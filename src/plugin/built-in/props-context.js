0a7ea38625dd582553a06a354a0e6cc161f6af5b
Fabio Ricali <f.ricali@xdevel.com>
2021-07-16 12:54:18 +0200

    implementing propsContext as array of props names

--- src/plugin/built-in/props-context.js
+++ src/plugin/built-in/props-context.js
@@ -2,7 +2,9 @@ module.exports = function(Doz, app) {
     app.on('componentCreate', component => {
         if (component.parent && component.parent.propsContext) {
             component.propsContext = component.parent.propsContext;
+            let propsContextIsArray = Array.isArray(component.propsContext);
             Object.keys(component.parent.props).forEach(propParent => {
+                if (propsContextIsArray && component.propsContext.indexOf(propParent) === -1) return
                 component.props[propParent] = component.parent.props[propParent];
             })
         }
@@ -10,9 +12,11 @@ module.exports = function(Doz, app) {
 
     app.on('componentUpdate', component => {
         if (component.propsContext) {
+            let propsContextIsArray = Array.isArray(component.propsContext);
             //console.log('a', component.tag, changes)
             Object.keys(component.children).forEach(child => {
                 Object.keys(component.props).forEach(propParent => {
+                    if (propsContextIsArray && component.propsContext.indexOf(propParent) === -1) return
                     component.children[child].props[propParent] = component.props[propParent];
                 })
             })
--- test/doz.props-context.js
+++ test/doz.props-context.js
@@ -10,7 +10,7 @@ describe('Doz.props-context', function () {
 
     describe('create app', function () {
 
-        it('should be ok', function (done) {
+        it('should be ok #1', function (done) {
 
             document.body.innerHTML = `<div id="app"></div>`;
 
@@ -82,6 +82,87 @@ describe('Doz.props-context', function () {
                 console.log(result);
 
                 if(!/Hello Lorem Ipsum/.test(result))
+                    throw new Error('Fail props-context #2')
+
+                done()
+
+            }, 100);
+
+        });
+        it('should be ok #2', function (done) {
+
+            document.body.innerHTML = `<div id="app"></div>`;
+
+            class SalutationCard extends Doz.Component{
+
+                constructor(o) {
+                    super(o);
+                    this.propsContext = [
+                        'name'
+                    ];
+                }
+
+                template(h) {
+                    return h`
+                        <${SalutationCardInner}/>
+                    `
+                }
+
+                onMount() {
+                    setTimeout(() => {
+                        this.props.myTitle = 'Lorem';
+                        this.props.name = 'Ipsum';
+                    }, 60)
+                }
+            }
+
+            class SalutationCardInner extends Doz.Component{
+
+                constructor(o) {
+                    super(o);
+                }
+
+                template(h) {
+                    return h`
+                        <div>Hello ${this.props.myTitle} ${this.props.name}</div>
+                    `
+                }
+            }
+
+            new Doz({
+                root: '#app',
+
+                propsListener: {
+                    desc: 'descHandler'
+                },
+
+                props: {
+                    desc: 'hello'
+                },
+
+                template(h) {
+                    return h`
+                        <${SalutationCard}
+                            my-title="MR."
+                            name="Doz">
+                        </>
+                    `
+                }
+            });
+
+            setTimeout(()=>{
+                const result = document.body.innerHTML;
+                console.log(result);
+
+                if(!/Hello undefined Doz/.test(result))
+                    throw new Error('Fail props-context #1')
+            }, 50);
+
+            setTimeout(()=>{
+                const result = document.body.innerHTML;
+                console.log(result);
+
+                if(!/Hello undefined Ipsum/.test(result))
                     throw new Error('Fail props-context #2')
 
                 done()
