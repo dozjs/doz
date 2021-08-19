6ebde346ad320a1cf4694eb5b15198bb4d25bc59
Fabio Ricali <ricalifabio@gmail.com>
2021-08-19 20:57:59 +0200

    v3.13.1

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 3.13.0  
+// [DOZ]  Build version: 3.13.1  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -4025,7 +4025,7 @@ Object.defineProperties(Doz, {
     enumerable: true
   },
   version: {
-    value: '3.13.0',
+    value: '3.13.1',
     enumerable: true
   },
   tag: {
