dff162b650f257e56ecddacf70894475dbc374a0
Fabio Ricali <f.ricali@xdevel.com>
2022-03-31 11:33:22 +0200

    fix test

--- test/doz.waitMount.js
+++ test/doz.waitMount.js
@@ -30,12 +30,14 @@ describe('Doz.waitMount', function () {
                         <other-tag></other-tag>
                     `
                 },
-                onWaitMount() {
+                async onWaitMount() {
                     console.log('onWaitMount')
-                    setTimeout(() => {
-                        this.runMount();
-                    }, 1000)
-
+                    await new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve()
+                        }, 1000)
+                    })
+                    this.runMount();
                 },
                 onMount() {
                     console.log('onMount')
@@ -49,12 +51,65 @@ describe('Doz.waitMount', function () {
                     `
                 },
                 onAppReady() {
+                    console.log('onAppReady')
                     //console.log(this.app._onAppComponentsMounted.size)
                     /*setInterval(() => {
                         this.app._onAppComponentsMounted.forEach(item => console.log(item))
                     },50)*/
 
                     //console.log('onComponentsMounted')
+                    done();
+                }
+            });
+        });
+
+        it('should be ok with doz.mount', function (done) {
+
+            document.body.innerHTML = `<div id="app"></div>`;
+
+            Doz.component('other-tag', {
+                waitMount: true,
+                template(h) {
+                    return h`
+                        <div>yeah</div>
+                    `
+                },
+                async onWaitMount() {
+                    console.log('onWaitMount')
+                    await new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve()
+                        }, 1000)
+                    })
+                    this.runMount();
+                },
+                onMount() {
+                    console.log('onMount')
+                }
+            })
+
+            Doz.component('salutation-card', {
+
+                template(h) {
+                    return h`
+                        <div>Hello</div>
+                        <other-tag></other-tag>
+                    `
+                }
+            });
+            new Doz({
+                root: '#app',
+                template(h) {
+                    return h`
+                        <div></div>
+                    `
+                },
+                onMount() {
+                    this.mount(this.h`<salutation-card></salutation-card>`)
+                },
+                onAppReady() {
+                    console.log('onAppReady')
+                    console.log(document.body.innerHTML);
                     done();
                 }
             });
