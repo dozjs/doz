95513476124d137a341ccce0bfa8c11f898fee92
Fabio Ricali <f.ricali@xdevel.com>
2022-04-15 16:14:03 +0200

    BUILD

--- dist/doz.js
+++ dist/doz.js
@@ -1,4 +1,4 @@
-// [DOZ]  Build version: 3.18.0  
+// [DOZ]  Build version: 3.18.1  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -4172,7 +4172,7 @@ Object.defineProperties(Doz, {
     enumerable: true
   },
   version: {
-    value: '3.18.0',
+    value: '3.18.1',
     enumerable: true
   },
   tag: {
